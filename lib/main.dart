5e9ef5f630474d3f21882f8a1a975a5d8ab79c26
Swagnik02 <swagnik1234@gmail.com>
2025-07-13 02:27:32 +0530

    optimised and refactored main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
@@ -11,57 +10,32 @@ import 'package:trexxo_mobility/utils/theme.dart';
 
 import 'firebase_options.dart';
 import 'utils/constants.dart';
-
-import 'package:trexxo_mobility/blocs/auth/auth_bloc.dart';
-import 'package:trexxo_mobility/blocs/auth/auth_event.dart';
-import 'package:trexxo_mobility/blocs/booking/booking_bloc.dart';
-import 'package:trexxo_mobility/cubits/location_cubit.dart';
-import 'package:trexxo_mobility/cubits/onboarding_cubit.dart';
-import 'package:trexxo_mobility/cubits/ride_request_cubit.dart';
 import 'cubits/theme_cubit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   final storage = await HydratedStorage.build(
     storageDirectory: HydratedStorageDirectory(
-      (await getTemporaryDirectory()).path,
+      (await getApplicationDocumentsDirectory()).path,
     ),
   );
 
   HydratedBloc.storage = storage;
 
   final firebaseService = FirebaseService();
+  final themeCubit = ThemeCubit();
 
-  runApp(
-    RepositoryProvider.value(
-      value: firebaseService,
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider(create: (_) => ThemeCubit()),
-          BlocProvider(create: (_) => OnboardingCubit()),
-          BlocProvider(create: (_) => LocationCubit()),
-          BlocProvider(create: (_) => BookingBloc()),
-          BlocProvider(create: (_) => RideRequestCubit()),
-          BlocProvider(
-            create:
-                (context) =>
-                    AuthBloc(firebaseService: context.read<FirebaseService>())
-                      ..add(AppStarted()),
-          ),
-        ],
-        child: const MyApp(),
-      ),
-    ),
-  );
+  runApp(buildRoot(firebaseService, themeCubit, const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ThemeCubit, ThemeMode>(
+      buildWhen: (previous, current) => previous != current,
       builder: (context, themeMode) {
         return MaterialApp(
           title: StringConstants.appFullName,
--- lib/root.dart
+++ lib/root.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:trexxo_mobility/blocs/auth/auth_bloc.dart';
+import 'package:trexxo_mobility/blocs/auth/auth_event.dart';
 import 'package:trexxo_mobility/blocs/auth/auth_state.dart';
+import 'package:trexxo_mobility/blocs/booking/booking_bloc.dart';
+import 'package:trexxo_mobility/cubits/location_cubit.dart';
 import 'package:trexxo_mobility/cubits/onboarding_cubit.dart';
+import 'package:trexxo_mobility/cubits/ride_request_cubit.dart';
+import 'package:trexxo_mobility/cubits/theme_cubit.dart';
+
 import 'package:trexxo_mobility/screens/auth/auth_screen.dart';
 import 'package:trexxo_mobility/screens/home/home_screen.dart';
 import 'package:trexxo_mobility/screens/onboarding/splash_screen.dart';
 import 'package:trexxo_mobility/screens/onboarding/welcome_screen.dart';
+
+import 'package:trexxo_mobility/services/firebase_service.dart';
+
+Widget buildRoot(
+  FirebaseService firebaseService,
+  ThemeCubit themeCubit,
+  Widget myApp,
+) {
+  return RepositoryProvider.value(
+    value: firebaseService,
+    child: MultiBlocProvider(
+      providers: [
+        BlocProvider.value(value: themeCubit),
+        BlocProvider(create: (_) => OnboardingCubit()),
+        BlocProvider(create: (_) => LocationCubit()),
+        BlocProvider(create: (_) => BookingBloc()),
+        BlocProvider(create: (_) => RideRequestCubit()),
+        BlocProvider(
+          create:
+              (context) =>
+                  AuthBloc(firebaseService: context.read<FirebaseService>())
+                    ..add(AppStarted()),
+        ),
+      ],
+      child: myApp,
+    ),
+  );
+}
 
 class RootInitializer extends StatefulWidget {
   const RootInitializer({super.key});
@@ -32,22 +67,27 @@ class _RootInitializerState extends State<RootInitializer> {
 
   @override
   Widget build(BuildContext context) {
-    if (_showSplash) {
-      return const SplashScreen();
-    }
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, authState) {
+        if (_showSplash) {
+          return const SplashScreen();
+        }
+        final isFirstTime = context.select<OnboardingCubit, bool>(
+          (cubit) => cubit.state,
+        );
 
-    final isFirstTime = context.watch<OnboardingCubit>().state;
-    if (isFirstTime) {
-      return WelcomeScreen();
-    } else {
-      final authState = context.watch<AuthBloc>().state;
-      if (authState is Authenticated) {
-        return const HomeScreen();
-      } else if (authState is Unauthenticated) {
-        return const AuthScreen();
-      } else {
-        return const SplashScreen();
-      }
-    }
+        if (authState is AuthInitial) {
+          return const SplashScreen();
+        }
+
+        if (isFirstTime) {
+          return const WelcomeScreen();
+        } else if (authState is Authenticated) {
+          return const HomeScreen();
+        } else {
+          return const AuthScreen();
+        }
+      },
+    );
   }
 }
